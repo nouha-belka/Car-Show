2757953c7b82afe7a22ec3c2986ec2837692002c
nouha belkacemi <nouhabelka2001@gmail.com>
2026-03-12 11:56:13 -0400

    car /create path working

--- src/main/java/com/example/Spring_MVC/controllers/CarController.java
+++ src/main/java/com/example/Spring_MVC/controllers/CarController.java
@@ -5,10 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.Spring_MVC.dto.CarRequest;
 import com.example.Spring_MVC.services.CarService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 
 
 @Slf4j
@@ -26,5 +28,15 @@ public class CarController {
         log.info("l22:mjm:carIndex: List of Cars Loaded");
         return "car/index";
     }
+
+    @GetMapping(value = {"/create", "/create/"})
+    public String carForm(Model model) {
+        model.addAttribute(
+            "newCar",
+            new CarRequest("", "", "", "", (byte) 0) 
+        );
+        return "car/form";
+    }
+    
     
 }
